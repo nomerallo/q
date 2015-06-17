13c97a2204ca3c5f28eb8fc5ce6be3bbfdd714fc
nomerallo <nomerallo@gmail.com>
2015-06-17 13:50:21 +0200

    Ejecución local, versión y despliegue en Heroku

--- models/models.js
+++ models/models.js
@@ -1,10 +1,35 @@
 var path = require('path');
 
+// Postgres DATABASE_URL = postgress://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+	{ dialect: protocol,
+	  protocol: protocol,
+	  port: port,
+	  host: host,
+	  storage: storage,	// solo SQLite (.env)
+	  omitNull: true	// solo Postgres
+	}
+);
+
+
 // Usar BBDD SQLite
-var sequelize = new Sequelize(null, null, null,	{ dialect: "sqlite", storage: "quiz.sqlite"	});
+//var sequelize = new Sequelize(null, null, null,	{ dialect: "sqlite", storage: "quiz.sqlite"	});
 
 // Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
