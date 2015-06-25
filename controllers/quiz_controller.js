c7efb8ee1c6e99b3d15e4e0f5522bc701cff9acd
nomerallo <nomerallo@gmail.com>
2015-06-25 13:33:10 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -92,3 +92,44 @@ exports.create = function(req, res){
 		}		// res.redirect: redirecciona HTTP a lista de preguntas
 	});	
 }*/
+
+
+// GET /quizes/:id/edit
+exports.edit = function(req,res){
+	var quiz = req.quiz;	// autoload de instancia de quiz
+
+	res.render('quizes/edit', { quiz: quiz, errors: []});
+}
+
+
+// PUT /quizes/:id
+exports.update = function(req,res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	var errors = req.quiz.validate();
+
+	if(errors){
+		var i=0; var errores = new Array();
+		for(var prop in errors) errores[i++]={message: errors[prop]};
+		res.render('quizes/edit', {quiz:req.quiz, errors: err.errors});
+	} else {
+		req.quiz
+			.save({fields: ["pregunta", "respuesta"]})
+			.then( function(){res.redirect('/quizes')});
+	}			
+}
+
+
+/*
+var errors = quiz.validate();//ya qe el objeto errors no tiene then(
+	if (errors){
+		var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+		for (var prop in errors) errores[i++]={message: errors[prop]};	
+		res.render('quizes/new', {quiz: quiz, errors: errores});
+	} else {
+		quiz // save: guarda en DB campos pregunta y respuesta de quiz
+		.save({fields: ["pregunta", "respuesta"]})
+		.then( function(){ res.redirect('/quizes')}) ;
+	}
+*/
